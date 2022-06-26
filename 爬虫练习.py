960c73e00fb97f2799d48ad2b7f1f5b8fff712d0
yeqi002 <1440968708@qq.com>
2022-06-26 23:33:28 +0800

    爬虫练习

--- 爬虫练习.py
+++ 爬虫练习.py
@@ -4,6 +4,9 @@ from urllib.request import urlopen
 
 myURL = urlopen("https://www.nipic.com/")
 with open("../PyDemo/test.txt", 'wb+') as f:
+    """
+    先写后读，在写完之后需要把游标移到文本开头
+    """
     f.write(myURL.read())
     f.seek(0)  # 将游标移动到文章开头
     # e=f.read()
