1b55d187c90ae7ea5dd6435ff4e47db47e71c56c
yeqi002 <1440968708@qq.com>
2022-06-26 23:36:15 +0800

    辅助

--- demo4.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-with open("../PyDemo/test.txt",'w+') as f:
-    #f.read()
-    #f.seek(0)
-    #print(f.read())
-    f.write("111122")
-    f.seek(0)
-    print(f.readline())
